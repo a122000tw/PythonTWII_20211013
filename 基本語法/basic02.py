76be97fc7b02e04510a01d2bceea1feaf9e58af8
a122000tw <a122000tw@gmail.com>
2021-10-20 19:39:07 +0800

    python

--- 基本語法/basic02.py
+++ 基本語法/basic02.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     r = 123
     # 求出園面積與球體積 (利用 math api 取得圓周率)
     # 請印出結果小數點到第二位
-    s = r**2*math.pi
+    area = (r**2)*math.pi
     v = 4/3*math.pi*(r**3)
-    print('圓面積=%.2f' % s, '球的體積=%.2f' % v)
+    print('圓面積=%.2f' % area, '球的體積=%.2f' % v)
