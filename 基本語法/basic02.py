840f27fe090837d67663b1348f05ff86f9391ac2
a122000tw <a122000tw@gmail.com>
2021-10-20 20:56:04 +0800

    python

--- 基本語法/basic02.py
+++ 基本語法/basic02.py
@@ -1,8 +1,15 @@
 import math
+
 if __name__ == '__main__':
     r = 123
-    # 求出園面積與球體積 (利用 math api 取得圓周率)
+    # 求出圓面積與球體積 (利用 math api 取得圓周率)
     # 請印出結果小數點到第二位
     area = (r**2)*math.pi
     v = 4/3*math.pi*(r**3)
-    print('圓面積=%.2f' % area, '球的體積=%.2f' % v)
+    print('圓面積=%.2f' % area, '球體積=%.2f' % v)
+    # 若要加上千分位
+    print(type(area))
+    # area = format(area, ",")
+    # print(area, type(area))
+    print('%.2f' % area)
+    print(format(float('%.2f' % area), ","))
---
+++ 基本語法/basic04.py
@@ -0,0 +1,8 @@
+
+if __name__ == '__main__':
+    symbol = '2230.TW'
+    amount = 3000
+    price = 614
+    # symbol=2330.TW&amount=3000&price=614
+    print(symbol, amount, price, sep='&', end=',')
+    print(symbol, amount, price, sep='&')
